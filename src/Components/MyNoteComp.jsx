bca81a5f5aecfa6324a0ffd5c1f3f3a7d940b36d
Enes Yağmur <106833747+enesyagmur@users.noreply.github.com>
2023-11-18 08:34:49 +0300

    home page görünümü

--- src/Components/MyNoteComp.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MyNoteComp = () => {
-  return <div>MyNoteComp</div>;
-};
-
-export default MyNoteComp;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import "./home.css";
-import NewNoteComp from "../Components/NewNoteComp";
+import { HiPencilAlt } from "react-icons/hi";
+import { MdNotes } from "react-icons/md";
+import { FaInstagram } from "react-icons/fa6";
+import { IoLogoGithub } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa";
 
 const Home = () => {
   return (
     <div className="home">
-      <NewNoteComp />
+      <div className="home-title">
+        <p>Welcome to</p> <p className="second-p">Notlab Web</p>
+      </div>
+      <button className="home-btn">
+        New Note <HiPencilAlt className="new-note-comp-btn-icon" />
+      </button>
+
+      <button className="home-btn">
+        My Note <MdNotes className="new-note-comp-btn-icon" />
+      </button>
+
+      <div className="home-social">
+        <FaInstagram className="icon" /> <IoLogoGithub className="icon" />{" "}
+        <FaLinkedin className="icon" />
+      </div>
     </div>
   );
 };
