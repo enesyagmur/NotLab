e51446101ff8244ddc013117d61b8fa8262cf8bb
Enes Yağmur <106833747+enesyagmur@users.noreply.github.com>
2023-12-05 08:34:53 +0300

    layz loading yapıldı

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./Pages/Login";
 import Home from "./Pages/Home";
-import Notes from "./Pages/Notes";
-import NewNote from "./Pages/NewNote";
+import React, { Suspense } from "react";
+
+const Login = React.lazy(() => import("./Pages/Login"));
+const Notes = React.lazy(() => import("./Pages/Notes"));
+const NewNote = React.lazy(() => import("./Pages/NewNote"));
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/notes" element={<Notes />} />
-          <Route path="/newnote" element={<NewNote />} />
-        </Routes>
+        <Suspense fallback={<p>Yükleniyor...</p>}>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/notes" element={<Notes />} />
+            <Route path="/newnote" element={<NewNote />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
